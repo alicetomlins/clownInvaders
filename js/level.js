87e5ab1762b0af65b582a18136defea2488c8b1a
alicetomlins <alice_tomlins@hotmail.co.uk>
2014-05-31 14:20:44 +0100

    Changes to sprite card & text on screen
    Correcting sprite card so correct aliens show

--- js/level.js
+++ js/level.js
@@ -8,7 +8,7 @@
           [0,0,0,0,0,0,0,0,0,0,0],
           [0,0,0,0,0,0,0,0,0,0,0],
           [0,0,0,0,0,0,0,0,0,0,0],
-          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,3,3,3,3,0,0,0],
           [0,0,0,0,2,2,2,2,0,0,0],
           [0,0,0,0,1,1,1,1,0,0,0]],
       2:  [[0,0,0,0,0,0,0,0,0,0,0],
@@ -27,9 +27,9 @@
 // Positions of graphics on the sprite card
 
   var spriteData = {
-    'alien1': { sx: 0,  sy: 0,  w: 23, h: 23, cls: Alien, frames: 2 },
-    'alien2': { sx: 0,  sy: 0, w: 23, h: 23, cls: Alien, frames: 2 },
-    'alien3': { sx: 0,  sy: 0, w: 23, h: 23, cls: Alien, frames: 2 },
+    'alien1': { sx: 0,  sy: 0,  w: 23, h: 23, cls: Alien, frames: 1 },
+    'alien2': { sx: 24,  sy: 0, w: 23, h: 23, cls: Alien, frames: 1 },
+    'alien3': { sx: 48,  sy: 0, w: 23, h: 23, cls: Alien, frames: 1 },
     'player': { sx: 0,  sy: 24, w: 22, h: 22, cls: Player },
     'missile': { sx: 2,  sy: 48, w: 5,  h: 5, cls: Missile }
   }
@@ -37,7 +37,7 @@
   
 // Start game callback screen - what appears on the start screen - links to engine .js
   function startGame() {
-    var screen = new GameScreen("Alien Invaders","press space to start",
+    var screen = new GameScreen("Clown Invaders","Please press space to begin",
                                  function() {
                                      Game.loadBoard(new GameBoard(1));
                                  });
@@ -47,7 +47,7 @@
 
 // End game callback screen - same as above but when the user dies
   function endGame() {
-    var screen = new GameScreen("Game Over","(press space to restart)",
+    var screen = new GameScreen("Game Over","Dare to try again? Press space!",
                                  function() {
                                      Game.loadBoard(new GameBoard(1));
                                  });
@@ -56,7 +56,7 @@
 
 // Win screen - what appears when the user wins
   function winGame() {
-    var screen = new GameScreen("You Win!","(press space to restart)",
+    var screen = new GameScreen("You Win!","Want another go? Press space!",
                                  function() {
                                      Game.loadBoard(new GameBoard(1));
                                  });
